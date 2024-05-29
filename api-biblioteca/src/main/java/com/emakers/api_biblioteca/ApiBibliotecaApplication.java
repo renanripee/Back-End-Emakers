7bf570f09b7714b1f4ceb2c6a6343a64d3af1023
Renan Ribeiro <renan.pereira1@estudante.ufla.br>
2024-05-29 10:56:30 -0300

    Criando o projeto Spring

---
+++ api-biblioteca/src/main/java/com/emakers/api_biblioteca/ApiBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.emakers.api_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBibliotecaApplication.class, args);
+	}
+
+}
